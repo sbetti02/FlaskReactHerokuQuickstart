c96929c325677e462d1ddeea964f3b25b3cd465b
Scott Bettigole <scott.bettigole@tufts.edu>
2020-12-25 23:10:17 -0500

    Update script so it confirms if the user meant to leave the API and/or client name unspecified

--- create_remote_apps.sh
+++ create_remote_apps.sh
@@ -14,8 +14,23 @@ while getopts ":a:c:" opt; do
   esac
 done
 
-# echo $api
-# echo $client
+# Confirm if API name is unspecified
+if [ -z "${api}" ]; then
+    read -p "Are you sure you want a random name for the API? y/n: " ans
+    if [ "${ans}" != "y" ]; then
+        echo "Please rerun the command with -a to specify the API name"
+        exit 0
+    fi
+fi
+
+# Confirm if client name is unspecified
+if [ -z "${client}" ]; then
+    read -p "Are you sure you want a random name for the React web client? y/n: " ans
+    if [ "${ans}" != "y" ]; then
+        echo "Please rerun the command with -c to specify the React web client name"
+        exit 0
+    fi
+fi
 
 # Create the API
 output=`heroku create $api`
@@ -41,7 +56,7 @@ echo $status
 
 while [ $status -ne 0 ]
 do
-    read -p "Please try a new name for the client: " client
+    read -p "Please try a new name for the React web client: " client
     echo $client
     output=`heroku create $client`
     status=$?
